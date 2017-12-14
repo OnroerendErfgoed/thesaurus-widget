90e84b7e3986d7c9a97708ff153365b992790a2a
Axel Verstappen <axel.verstappen@gmail.com>
2017-12-14 15:54:10 +0100

    Completed interfaces and removed interface prefix tslint

--- src/models/apiModel.ts
+++ src/models/apiModel.ts
@@ -11,112 +11,54 @@ export interface ITreeConcept {
 }
 
 export interface ICollection {
-  
+  id: number;
+  labels: ILabel[];
+  members: IMember[];
+  member_of: IMember[];
+  concept_scheme: IConceptScheme;
+  uri: string;
+  label: string;
+  type: string;
+  sources: any[];
+  notes: any[];
+  superordinates: any[];
 }
 
 export interface IConcept {
-  
+  id: number;
+  labels: ILabel[];
+  member_of: IMember[];
+  concept_scheme: IConceptScheme;
+  uri: string;
+  label: string;
+  type: string;
+  notes: any[];
+  subordinate_arrays: any[];
+  matches: IMatches;
 }
 
-/* COLLECTION
-{
-    "labels": [
-        {
-            "type": "prefLabel",
-            "language": "nl",
-            "label": "beschermd erfgoed"
-        }
-    ],
-    "sources": [],
-    "members": [
-        {
-            "label": "beschermd monument",
-            "type": "concept",
-            "id": 1,
-            "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/1"
-        },
-        {
-            "label": "beschermd cultuurhistorisch landschap",
-            "type": "concept",
-            "id": 2,
-            "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/2"
-        },
-        {
-            "label": "beschermd stads- of dorpsgezicht",
-            "type": "concept",
-            "id": 3,
-            "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/3"
-        },
-        {
-            "label": "beschermde archeologische site",
-            "type": "concept",
-            "id": 4,
-            "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/4"
-        },
-        {
-            "label": "beschermd varend erfgoed",
-            "type": "concept",
-            "id": 5,
-            "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/5"
-        },
-        {
-            "label": "overgangszone",
-            "type": "concept",
-            "id": 6,
-            "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/6"
-        }
-    ],
-    "id": 15,
-    "concept_scheme": {
-        "labels": [],
-        "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes"
-    },
-    "member_of": [],
-    "notes": [],
-    "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/15",
-    "label": "beschermd erfgoed",
-    "superordinates": [],
-    "type": "collection"
+export interface ILabel {
+  type: string;
+  language: string;
+  label: string;
 }
-*/
 
-/* CONCEPT
-{
-    "subordinate_arrays": [],
-    "matches": {
-        "close": [],
-        "narrow": [],
-        "exact": [],
-        "related": [],
-        "broad": []
-    },
-    "labels": [
-        {
-            "type": "prefLabel",
-            "language": "nl",
-            "label": "beschermd varend erfgoed"
-        }
-    ],
-    "narrower": [],
-    "related": [],
-    "sources": [],
-    "broader": [],
-    "id": 5,
-    "concept_scheme": {
-        "labels": [],
-        "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes"
-    },
-    "member_of": [
-        {
-            "label": "beschermd erfgoed",
-            "type": "collection",
-            "id": 15,
-            "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/15"
-        }
-    ],
-    "notes": [],
-    "uri": "https://id.erfgoed.net/thesauri/aanduidingstypes/5",
-    "label": "beschermd varend erfgoed",
-    "type": "concept"
+export interface IMember {
+  label: string;
+  type: string;
+  id: number;
+  uri: string;
 }
-*/
+
+export interface IConceptScheme {
+  labels: ILabel[];
+  uri: string;
+}
+
+export interface IMatches {
+  close: any[];
+  narrow: any[];
+  exact: any[];
+  related: any[];
+  broad: any[];
+}
