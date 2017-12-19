7f52d3ef5d16aa0393f3a6d1b20dc5536f849298
Vanderhaegen Cedrik <cedrik.vanderhaegen@vlaanderen.be>
2017-12-19 14:48:38 +0100

    add simple api test

--- test/unit/api-service.spec.ts
+++ test/unit/api-service.spec.ts
@@ -2,45 +2,56 @@ import { HttpClientMock } from 'aurelia-http-client-mock';
 import { ApiService } from '../../src/services/api-service';
 
 describe('the ApiService module', () => {
-  const http = new HttpClientMock();
-  const sut = new ApiService(http);
+
+  let httpMock;
+  let sut;
+
   const testData = [
     {
-        children: [
-            {
-                children: [],
-                type: 'concept',
-                id: '1.2',
-                concept_id: 2,
-                label: 'pijpaarde'
-            },
-            {
-                children: [],
-                type: 'concept',
-                id: '1.5',
-                concept_id: 5,
-                label: 'porselein'
-            },
-            {
-                children: [],
-                type: 'concept',
-                id: '1.6',
-                concept_id: 6,
-                label: 'steengoed'
-            }
-        ],
-        type: 'concept',
-        id: '1',
-        concept_id: 1,
-        label: 'aardewerk'
+      children: [
+        {
+          children: [],
+          type: 'concept',
+          id: '1.2',
+          concept_id: 2,
+          label: 'pijpaarde'
+        },
+        {
+          children: [],
+          type: 'concept',
+          id: '1.5',
+          concept_id: 5,
+          label: 'porselein'
+        },
+        {
+          children: [],
+          type: 'concept',
+          id: '1.6',
+          concept_id: 6,
+          label: 'steengoed'
+        }
+      ],
+      type: 'concept',
+      id: '1',
+      concept_id: 1,
+      label: 'aardewerk'
     }
   ];
 
-  it('saves a record', (done) => {
-    sut.getTree().then((record) => {
-      console.log(record);
-      expect(record).toEqual(testData);
-      done();
-    });
+  beforeEach(() => {
+    httpMock = new HttpClientMock();
+    sut = new ApiService(httpMock);
+  });
+
+  it('should call the correct base url', done => {
+    httpMock.expect('https://dev-thesaurus.onroerenderfgoed.be/conceptschemes/MATERIALEN/c')
+      .withMethod('GET')
+      .withResponseStatus(200);
+
+    sut.getConcepts()
+      .then(response  => {
+        expect(response).toBe('');
+        done();
+      });
   });
 });
