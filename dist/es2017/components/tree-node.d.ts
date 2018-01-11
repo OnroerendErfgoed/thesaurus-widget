85f7532db0510c482e7728ee78fcb9bd43182213
Axel Verstappen <axel.verstappen@gmail.com>
2018-01-11 11:48:26 +0100

    Remove possibly unnecessary components

--- dist/es2017/components/tree-node.d.ts
+++
@@ -1,4 +0,0 @@
-import { TreeChild } from '../models/tree';
-export declare class TreeNode {
-    current: TreeChild;
-}
--- src/components/tree-node.ts
+++
@@ -1,6 +0,0 @@
-import { bindable } from 'aurelia-framework';
-import { TreeChild } from '../models/tree';
-
-export class TreeNode {
-  @bindable public current: TreeChild = null;
-}
